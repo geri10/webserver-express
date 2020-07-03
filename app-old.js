e7c57db59a92666cb8e9086eb9bd47a6e93eef81
yerson <yerson.fuentes2012@gmail.com>
2020-07-03 00:42:39 -0400

    primer commit

---
+++ app-old.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        let salida = {
+            nombre: 'yerson',
+            edade: 25,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida));
+        //res.write('Hola Mundo');
+        res.end();
+
+    })
+    .listen(8080);
+
+console.log('escuchando el puerto 8080');
